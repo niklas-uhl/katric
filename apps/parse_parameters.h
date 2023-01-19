0926d8d7db2afaf18fc8cd30b9ef7a1e8f13ae40
Tim Niklas Uhl <uhl@kit.edu>
2023-01-19 18:01:53 +0100

    Some cleanup.

--- apps/parse_parameters.h
+++ apps/parse_parameters.h
@@ -1,9 +1,4 @@
-//
-// Created by Tim Niklas Uhl on 17.11.20.
-//
-
-#ifndef PARALLEL_TRIANGLE_COUNTER_PARSE_PARAMETERS_H
-#define PARALLEL_TRIANGLE_COUNTER_PARSE_PARAMETERS_H
+#pragma once
 
 #include <memory>
 #include <string>
@@ -12,6 +7,7 @@
 #include <CLI/Validators.hpp>
 
 #include "cetric/config.h"
+#include "git_hash.h"
 #include "magic_enum.hpp"
 
 inline std::shared_ptr<CLI::App> parse_parameters(const std::string& app_name, cetric::Config& conf) {
@@ -21,15 +17,15 @@ inline std::shared_ptr<CLI::App> parse_parameters(const std::string& app_name, c
     return app;
 }
 
-template<typename E>
+template <typename E>
 constexpr auto enum_name_to_value_map() {
-  std::map<std::string, E> mapping;
-  constexpr auto values = magic_enum::enum_values<E>();
-  constexpr auto names = magic_enum::enum_names<E>();
-  for (unsigned i = 0; i < magic_enum::enum_count<E>(); ++i) {
-    mapping[std::string(names[i])] = values[i];
-  }
-  return mapping;
+    std::map<std::string, E> mapping;
+    constexpr auto           values = magic_enum::enum_values<E>();
+    constexpr auto           names  = magic_enum::enum_names<E>();
+    for (unsigned i = 0; i < magic_enum::enum_count<E>(); ++i) {
+        mapping[std::string(names[i])] = values[i];
+    }
+    return mapping;
 }
 
 #ifndef CLI11_PARSE_MPI
@@ -53,9 +49,7 @@ constexpr auto enum_name_to_value_map() {
 inline void parse_gen_parameters(CLI::App& app, cetric::Config& conf) {
     app.add_option("--seed", conf.gen.seed);
     app.add_option("--gen", conf.gen.generator)
-        ->transform(
-            CLI::IsMember({"gnm", "rdg_2d", "rdg_3d", "rgg_2d", "rgg_3d", "rhg", "ba", "grid_2d", "rmat"})
-        );
+        ->transform(CLI::IsMember({"gnm", "rdg_2d", "rdg_3d", "rgg_2d", "rgg_3d", "rhg", "ba", "grid_2d", "rmat"}));
     app.add_option("--gen_n", conf.gen.n);
     app.add_option("--gen_m", conf.gen.m);
     // app.add_option("--gen_r", conf.gen_r);
@@ -70,4 +64,84 @@ inline void parse_gen_parameters(CLI::App& app, cetric::Config& conf) {
     app.add_flag("--gen_statistics", conf.gen.statistics);
 }
 
-#endif // PARALLEL_TRIANGLE_COUNTER_PARSE_PARAMETERS_H
+namespace shmetric {
+cetric::Config parse_config(int argc, char* argv[]) {
+    CLI::App app("Parallel Triangle Counter");
+    app.option_defaults()->always_capture_default();
+    cetric::Config conf;
+    conf.git_commit = cetric::git_hash;
+
+    // conf.hostname = std::getenv("HOST");
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    conf.hostname = hostname;
+
+    app.add_option("input", conf.input_file, "The input graph");
+
+    app.add_option("--input-format", conf.input_format)
+        ->transform(CLI::CheckedTransformer(graphio::input_types, CLI::ignore_case));
+    app.add_option("--partitioning", conf.partitioning);
+
+    app.add_option("--iterations", conf.iterations);
+
+    app.add_flag("-v", conf.verbosity_level, "verbosity level");
+
+    app.add_option("--json-output", conf.json_output);
+
+    app.add_flag("--degree-filtering", conf.degree_filtering);
+
+    app.add_option("--num-threads", conf.num_threads);
+    app.add_option("--grainsize", conf.grainsize);
+    app.add_option("--local-degree-of-parallelism", conf.local_degree_of_parallelism);
+
+    app.add_flag("--pseudo2core", conf.pseudo2core);
+
+    app.add_flag("--id-node-ordering", conf.id_node_ordering);
+
+    app.add_flag("--flag-intersection", conf.flag_intersection);
+
+    app.add_flag("--skip-local-neighborhood", conf.skip_local_neighborhood);
+
+    conf.local_parallel = true;
+    // app.add_flag("--local-parallel", conf.local_parallel);
+
+    app.add_option("--parallelization-method", conf.parallelization_method)
+        ->transform(CLI::CheckedTransformer(enum_name_to_value_map<cetric::ParallelizationMethod>(), CLI::ignore_case));
+    app.add_flag("--edge-partitioning", conf.edge_partitioning);
+    app.add_flag("--edge-partitioning-static", conf.edge_partitioning_static);
+    app.add_option("--omp-schedule", conf.omp_schedule)
+        ->transform(CLI::CheckedTransformer(enum_name_to_value_map<cetric::OMPSchedule>(), CLI::ignore_case));
+    app.add_option("--omp-chunksize", conf.omp_chunksize);
+
+    app.add_option("--tbb-partitioner", conf.tbb_partitioner)
+        ->transform(CLI::CheckedTransformer(enum_name_to_value_map<cetric::TBBPartitioner>(), CLI::ignore_case));
+
+    app.add_option("--high-degree-threshold-scale", conf.high_degree_threshold_scale);
+
+    app.add_option("--intersection-method", conf.intersection_method)
+        ->transform(CLI::CheckedTransformer(cetric::intersection_method_map, CLI::ignore_case));
+    app.add_option("--binary-intersection-cutoff", conf.binary_intersection_cutoff);
+    app.add_option("--hybrid-cutoff-scale", conf.hybrid_cutoff_scale);
+
+    parse_gen_parameters(app, conf);
+
+    CLI::Option* input_option = app.get_option("input");
+    CLI::Option* gen_option   = app.get_option("--gen");
+    input_option->excludes(gen_option);
+    gen_option->excludes(input_option);
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI ::ParseError& e) {
+        app.exit(e);
+    }
+    if (conf.input_file.empty() && conf.gen.generator.empty()) {
+        CLI::RequiredError e("Provide an input file via 'input' or using the "
+                             "generator via '--gen'");
+        exit(app.exit(e));
+    }
+    conf.rank = 0;
+    conf.PEs  = 1;
+    return conf;
+}
+} // namespace shmetric
--- apps/shmetric.cpp
+++ apps/shmetric.cpp
@@ -63,88 +63,10 @@ void print_summary(
         }
     }
 }
-cetric::Config parse_config(int argc, char* argv[]) {
-    CLI::App app("Parallel Triangle Counter");
-    app.option_defaults()->always_capture_default();
-    cetric::Config conf;
-    conf.git_commit = cetric::git_hash;
 
-    // conf.hostname = std::getenv("HOST");
-    char hostname[256];
-    gethostname(hostname, sizeof(hostname));
-    conf.hostname = hostname;
-
-    app.add_option("input", conf.input_file, "The input graph");
-
-    app.add_option("--input-format", conf.input_format)
-        ->transform(CLI::CheckedTransformer(graphio::input_types, CLI::ignore_case));
-    app.add_option("--partitioning", conf.partitioning);
-
-    app.add_option("--iterations", conf.iterations);
-
-    app.add_flag("-v", conf.verbosity_level, "verbosity level");
-
-    app.add_option("--json-output", conf.json_output);
-
-    app.add_flag("--degree-filtering", conf.degree_filtering);
-
-    app.add_option("--num-threads", conf.num_threads);
-    app.add_option("--grainsize", conf.grainsize);
-    app.add_option("--local-degree-of-parallelism", conf.local_degree_of_parallelism);
-
-    app.add_flag("--pseudo2core", conf.pseudo2core);
-
-    app.add_flag("--id-node-ordering", conf.id_node_ordering);
-
-    app.add_flag("--flag-intersection", conf.flag_intersection);
-
-    app.add_flag("--skip-local-neighborhood", conf.skip_local_neighborhood);
-
-    conf.local_parallel = true;
-    // app.add_flag("--local-parallel", conf.local_parallel);
-
-    app.add_option("--parallelization-method", conf.parallelization_method)
-        ->transform(CLI::CheckedTransformer(enum_name_to_value_map<cetric::ParallelizationMethod>(), CLI::ignore_case));
-    app.add_flag("--edge-partitioning", conf.edge_partitioning);
-    app.add_flag("--edge-partitioning-static", conf.edge_partitioning_static);
-    app.add_option("--omp-schedule", conf.omp_schedule)
-        ->transform(CLI::CheckedTransformer(enum_name_to_value_map<cetric::OMPSchedule>(), CLI::ignore_case));
-    app.add_option("--omp-chunksize", conf.omp_chunksize);
-
-    app.add_option("--tbb-partitioner", conf.tbb_partitioner)
-        ->transform(CLI::CheckedTransformer(enum_name_to_value_map<cetric::TBBPartitioner>(), CLI::ignore_case));
-
-    app.add_option("--high-degree-threshold-scale", conf.high_degree_threshold_scale);
-
-    app.add_option("--intersection-method", conf.intersection_method)
-        ->transform(CLI::CheckedTransformer(cetric::intersection_method_map, CLI::ignore_case));
-    app.add_option("--binary-intersection-cutoff", conf.binary_intersection_cutoff);
-    app.add_option("--hybrid-cutoff-scale", conf.hybrid_cutoff_scale);
-
-    parse_gen_parameters(app, conf);
-
-    CLI::Option* input_option = app.get_option("input");
-    CLI::Option* gen_option   = app.get_option("--gen");
-    input_option->excludes(gen_option);
-    gen_option->excludes(input_option);
-
-    try {
-        app.parse(argc, argv);
-    } catch (const CLI ::ParseError& e) {
-        app.exit(e);
-    }
-    if (conf.input_file.empty() && conf.gen.generator.empty()) {
-        CLI::RequiredError e("Provide an input file via 'input' or using the "
-                             "generator via '--gen'");
-        exit(app.exit(e));
-    }
-    conf.rank = 0;
-    conf.PEs  = 1;
-    return conf;
-}
 int main(int argc, char* argv[]) {
     backward::SignalHandling sh;
-    auto                     conf            = parse_config(argc, argv);
+    auto                     conf            = shmetric::parse_config(argc, argv);
     size_t                   max_concurrency = tbb::this_task_arena::max_concurrency();
     if (conf.num_threads == 0) {
         conf.num_threads = max_concurrency;
@@ -178,41 +100,31 @@ int main(int argc, char* argv[]) {
             break;
     }
 
-    std::optional<double>                      io_time;
-    cetric::profiling::Timer                   t;
-    cetric::graph::DistributedGraph<>          G{graphio::read_graph(conf.input_file, conf.input_format)};
+    std::optional<double>             io_time;
+    cetric::profiling::Timer          t;
+    cetric::graph::DistributedGraph<> G{graphio::read_graph(conf.input_file, conf.input_format)};
+    // compute the number of wedges
+    tbb::enumerable_thread_specific<size_t> thread_local_wedges = 0;
+    tbb::parallel_for(tbb::blocked_range(G.local_nodes().begin(), G.local_nodes().end()), [&](auto const& r) {
+        auto& wedges_local = thread_local_wedges.local();
+        for (auto node: r) {
+            auto degree = G.degree(node);
+            wedges_local += (degree * (degree - 1)) / 2;
+        }
+    });
+    size_t wedges = thread_local_wedges.combine(std::plus<>{});
+
     std::vector<cetric::profiling::Statistics> all_stats;
     for (size_t iter = 0; iter < conf.iterations; ++iter) {
         cetric::profiling::Statistics stats(0, 1);
         cetric::profiling::Timer      timer;
-        // LOG << "[R" << rank << "] "
-        //     << "Loading from cache";
-        // graphio::IOResult input = input_cache.get();
-        // LOG << "[R" << rank << "] "
-        //     << "Finished loading from cache";
-        // // atomic_debug(G_local.node_info);
-        //  atomic_debug(G_local.edge_heads);
-        // G = cetric::graph::DistributedGraph<>(
-        //     std::move(input.G),
-        //     {input.info.local_from, input.info.local_to},
-        //     rank,
-        //     size
-        // );
-        tbb::enumerable_thread_specific<size_t> wedges = 0;
-        tbb::parallel_for(tbb::blocked_range(G.local_nodes().begin(), G.local_nodes().end()), [&](auto const& r) {
-            for (auto node: r) {
-                auto degree = G.degree(node);
-                wedges.local() += (degree * (degree - 1)) / 2;
-            }
-        });
-        stats.local.wedges = wedges.combine(std::plus<>{});
+        stats.local.wedges = wedges;
         // atomic_debug(G);
         stats.local.io_time = timer.elapsed_time();
         cetric::profiling::Timer global_time;
 
         cetric::run_shmem(G, stats, conf);
 
-        // MPI_Barrier(MPI_COMM_WORLD);
         stats.local.local_wall_time = global_time.elapsed_time();
 
         stats.collapse();
